1eb7571563c498b60b537a9a2c53894552eb3bb5
Vinson 2 <vinsonhe2@gmail.com>
2025-05-26 20:51:24 -0700

    Create first react component

---
+++ src/Greeting.jsx
@@ -0,0 +1,8 @@
+function TestHeading() {
+    return <h1>
+        This is a heading created using JSX!
+        "She telling me this, and telling me that"
+    </h1>;
+}
+
+export default TestHeading;
