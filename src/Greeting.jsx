528afde6f284e84f36adb6f48d5474b6a52b3aeb
Vinson 2 <vinsonhe2@gmail.com>
2025-05-27 00:25:48 -0700

    Add new components to website

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,8 +1,27 @@
+import helloGif from './assets/hello.gif';
+import bossam from './assets/bossam.jpeg';
+import './greeting.css';
+
+function HelloGif() {
+    return <img src={helloGif} className="hello-gif" alt="" />
+}
+
 function TestHeading() {
     return <h1>
         This is a heading created using JSX!
-        "She telling me this, and telling me that"
     </h1>;
 }
 
-export default TestHeading;
+function FavoriteFood() {
+    return <div className='favorite-food-div'>
+        <h2>Now let me tell you about my favorite food right now...</h2>
+        <p className='food-para'>It's Bossam from South Korea!</p>
+        <p className='food-para'>
+            Bossam is a dish made with boiled pork belly (with spices!) and served with sides 
+            such as napa cabbage, kimchi, or more!
+        </p>
+        <img src={bossam} className='food-image' alt='Plated bossam consisting of sliced pork, fresh lettuce, and kimchi' />
+    </div>
+}
+
+export { HelloGif, TestHeading, FavoriteFood };
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import TestHeading from './Greeting.jsx'
+import { FavoriteFood, HelloGif, TestHeading } from './Greeting.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HelloGif />
     <TestHeading />
+    <FavoriteFood />
   </StrictMode>,
 )
